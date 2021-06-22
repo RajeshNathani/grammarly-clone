846ff41e97ff372740934fa1820e653723e208c4
RajeshNathani <rajeshnathani055@gmail.com>
2021-06-22 11:03:12 +0530

    build

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,29 @@
 import Head from 'next/head'
-import Styles from '../styles/Home.module.css'
-
+import Link from 'next/link'
 const index = () => {
     return (
         <div style={{backgroundImage: '-webkit-linear-gradient(90deg,#fff 3%,hsla(0,0%,100%,0)),-webkit-linear-gradient(347deg,#aaf2e1,#93edf5 50%,#dbebff)' , backgroundPosition: 'right'}}>
             <Head>
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link>
             </Head>
             <nav className="navbar navbar-light">
-                <a className="navbar-brand" href="/"><b>Grammarly Clone</b></a>
+            <Link href="/"> 
+                <a className="navbar-brand"><b>Grammarly Clone</b></a>
+            </Link>
             </nav><br/>
             <div className='container d-lg-flex justify-content-around align-items-center p' style={{height: 100 + 'vh'}}>
                 <div>
                     <h1>Great Writing, <br/> Simplified</h1><br/>
                     <p className='lead ' style={{fontWeight: 400 , fontSize: 20+'px'}}>Compose bold, clear, mistake-free writing with <br/> Grammarly Clones’s AI-powered writing assistant.</p>
                     <br/><br/>
-                    <a href='/app' className='btn btn-success' style={{width: 200+'px'}}>Try Now</a><br/><br/>
+                    <Link href="/app"> 
+                        <a className='btn btn-success' style={{width: 200+'px'}}>Try Now</a>
+                    </Link><br/><br/>
                 </div>
                 <div style={{height: 50+'vh' , width: 40+'%', boxShadow: 0 +"px " + 0 + "px " + 3 +"px " +'black'}} className='d-flex flex-column container a'>
                     <br/><p className='lead' style={{fontWeight: 400 , fontSize: 20+'px'}}>Grammarly Clone</p> <hr color='black' width='100%'></hr>    <br/><br/>               
                     <p className='lead' style={{fontWeight: 400 , fontSize: 20+'px'}}>My My name is ABCD...</p> <br/>
-                    <p className='lead text-danger' style={{fontWeight: 400 , fontSize: 20+'px'}}><b>Error: 'My' is repeated.</b></p>    <br/><br/>                
+                    <p className="lead text-danger" style={{fontWeight: 400 , fontSize: 20+"px"}}><b>Error: &quot; My &quot; is repeated.</b></p>    <br/><br/>                
                 </div>
             </div>
             
